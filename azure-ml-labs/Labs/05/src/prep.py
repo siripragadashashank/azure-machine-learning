3000935fce09f7e6a5a6d0be96983d63117d6990
Shashank Siripragada <siripragada.s@northeastern.edu>
2024-06-18 00:44:56 -0400

    create component contd. in Lab09

---
+++ azure-ml-labs/Labs/05/src/prep.py
@@ -0,0 +1,35 @@
+# import libraries
+import argparse
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from sklearn.preprocessing import MinMaxScaler
+
+# setup arg parser
+parser = argparse.ArgumentParser()
+
+# add arguments
+parser.add_argument("--input_data", dest='input_data',
+                    type=str)
+parser.add_argument("--output_data", dest='output_data',
+                    type=str)
+
+# parse args
+args = parser.parse_args()
+
+# read the data
+df = pd.read_csv(args.input_data)
+
+# remove missing values
+df = df.dropna()
+
+# normalize the data
+scaler = MinMaxScaler()
+num_cols = ['feature1','feature2','feature3','feature4']
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+# save the data as a csv
+output_df = df.to_csv(
+    (Path(args.output_data) / "prepped-data.csv"),
+    index = False
+)
